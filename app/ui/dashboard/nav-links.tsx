e27d12cc58a9e732d5ecb7ab925105603079e8e9
Ali Raza <aliraza512joker@gmail.com>
2024-10-15 04:35:42 +0500

    commit done

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,30 +1,24 @@
-'use client'
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link'
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
-  {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-];
+
+// ...
 
 export default function NavLinks() {
   const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-
           <Link
             key={link.name}
             href={link.href}
@@ -42,4 +36,8 @@ export default function NavLinks() {
       })}
     </>
   );
-}
+}const links = [
+  { name: 'Home', href: '/', icon: HomeIcon },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Invoices', href: '/dashboard/invoices', icon: DocumentDuplicateIcon },
+];
